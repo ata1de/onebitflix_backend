1c68191be91b68bf1f245114d11607503f5f212f
unknown <teteulima@outlook.com>
2024-03-19 11:41:53 -0300

    created middlewares of auth for all routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import { categoriesController } from './controllers/categoryController'
 import { coursesController } from './controllers/coursesController'
 import { authController } from './controllers/authController'
+import { ensureAuth, ensureAuthViaQuery } from './middlewares/auth'
 
 
 const router = express.Router()
@@ -11,15 +12,15 @@ router.post('/auth/register', authController.register)
 router.post('/auth/login', authController.login)
 
 // GET
-router.get('/categories', categoriesController.index)
-router.get('/categories/:id', categoriesController.show)
+router.get('/categories', ensureAuth ,categoriesController.index)
+router.get('/categories/:id',ensureAuth , categoriesController.show)
 
-router.get('/courses/featured', coursesController.featured)
-router.get('/courses/newest', coursesController.newest)
-router.get('/courses/search', coursesController.search)
-router.get('/courses/:id', coursesController.show)
+router.get('/courses/featured', ensureAuth ,coursesController.featured)
+router.get('/courses/newest' ,coursesController.newest)
+router.get('/courses/search', ensureAuth , coursesController.search)
+router.get('/courses/:id', ensureAuth , coursesController.show)
 
-router.get('/episodes/stream', coursesController.search)
+router.get('/episodes/stream', ensureAuthViaQuery,coursesController.search)
 
 
 export { router }
