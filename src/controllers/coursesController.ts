0b5885173ff46a2f076b10bbf580387024af6732
unknown <teteulima@outlook.com>
2024-03-20 17:25:16 -0300

    created new route of the show top 10 courses

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -67,7 +67,19 @@ const coursesController = {
                 res.status(400).json({message: error.message})
             }
         }
-    } 
+    },
+
+     // GET /courses/popular
+  popular: async (req: Request, res: Response) => {
+    try {
+      const topTen = await courseService.getTopTenByLikes()
+      return res.json(topTen)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  }
 
 }
 
