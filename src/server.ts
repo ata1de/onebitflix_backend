bbe619907089331167b9401d57ba3a8e7666ec8d
unknown <teteulima@outlook.com>
2024-03-21 11:49:10 -0300

    added cors

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,10 @@ import express from 'express'
 import { database } from './database'
 import { adminJs, adminJsRouter } from './adminjs'
 import { router } from './routes'
-
+import cors from 'cors'
 const app = express()
+
+app.use(cors())
 
 app.use(express.static('public'))
 
