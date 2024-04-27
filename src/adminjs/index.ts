027369b53c806af1382812cf398fb88270f5ee9a
unknown <teteulima@outlook.com>
2024-04-27 19:30:54 -0300

    finished preparing for deployment

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -5,12 +5,19 @@ import AdminJsExpress from '@adminjs/express'
 import AdminJsSequelize from '@adminjs/sequelize'
 import { database } from '../database'
 import { adminJsResources } from './resources'
-// src/adminjs/index.ts
-
-// ...
 import { dashboardOptions } from './dashboard'
 import { brandingOptions } from './branding'
 import { authtenticationOptions } from './authentication'
+// libs instaladas para tirar o armazenamento na memoria para adiciionar as sessoes do usuário no Admin
+import session from 'express-session'
+import connectSession from 'connect-session-sequelize'
+import { Sequelize } from 'sequelize'
+import { ADMINJS_COOKIE_PASSWORD } from '../config/enviroment'
+
+const SequelizeStore = connectSession(session.Store)
+const store = new SequelizeStore({ db: database})
+// metodo para sincronizar para criar a tabela de gerenciamento de sessoes
+store.sync()
 
 AdminJs.registerAdapter(AdminJsSequelize)
 
@@ -26,5 +33,9 @@ export const adminJsRouter = AdminJsExpress.buildAuthenticatedRouter(
   adminJs,
   authtenticationOptions,
   null,
-  { resave: false, saveUninitialized: false }
+  { resave: false,
+    saveUninitialized: false,
+    store: store,
+    secret: ADMINJS_COOKIE_PASSWORD
+    }
 )
